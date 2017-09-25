33a074cab522466761aa33eae36afd2bbe09f943
Christian <christiansamuelpaul@gmail.com>
2017-09-25 19:26:52 -0400

    add lazy loading

--- js/index.js
+++ js/index.js
@@ -1,3 +1,48 @@
+// lazy loading code taken from http://deanhume.com/home/blogpost/lazy-loading-images-using-intersection-observer/10163
+
+// Get all of the images that are marked up to lazy load
+const images = document.querySelectorAll('.js-lazy-image');
+const config = {
+  // If the image gets within 50px in the Y axis, start the download.
+  rootMargin: '50px 0px',
+  threshold: 0.01
+};
+
+// The observer for the images on the page
+let observer = new IntersectionObserver(onIntersection, config);
+  images.forEach(image => {
+    observer.observe(image);
+  });
+
+function onIntersection(entries) {
+  // Loop through the entries
+  entries.forEach(entry => {
+    // Are we in viewport?
+    if (entry.intersectionRatio > 0) {
+
+      // Stop watching and load the image
+      observer.unobserve(entry.target);
+      preloadImage(entry.target);
+    }
+  });
+}
+
+// If we don't have support for intersection observer, load the images immediately
+if (!('IntersectionObserver' in window)) {
+  Array.from(images).forEach(image => preloadImage(image));
+} else {
+  // It is supported, load the images
+  observer = new IntersectionObserver(onIntersection, config);
+  images.forEach(image => {
+ 
+   observer.observe(image);
+  });
+}
+
+function preloadImage(img) {
+    img.setAttribute('src', img.getAttribute('data-src'));
+}
+
 // smooth scrolling to page sections
 $('.nav-about').click(function() {
 	$('html, body').animate(
@@ -13,13 +58,6 @@ $('.nav-portfolio').click(function() {
 		}, 1400)
 });
 
-$('.nav-contact').click(function() {
-	$('html, body').animate(
-		{
-			scrollTop: $('.contact-holder').offset().top
-		}, 2000)
-});
-
 $('.footer-about').click(function() {
 	$('html, body').animate(
 		{
@@ -32,13 +70,6 @@ $('.footer-portfolio').click(function() {
 		{
 			scrollTop: $('.portfolio-holder').offset().top
 		}, 1800)
-});
-
-$('.footer-contact').click(function() {
-	$('html, body').animate(
-		{
-			scrollTop: $('.contact-holder').offset().top
-		}, 1000)
 });
 
 // make portfolio rows the same height
